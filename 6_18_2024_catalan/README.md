26ff8b4eb6d63bf73a45b0f462f51c26e624a479
Herman Chau <beijingherman@gmail.com>
2024-06-17 20:59:39 -0700

    Initial commit

---
+++ 6_18_2024_catalan/README.md
@@ -0,0 +1,12 @@
+# Catalan Objects
+In this assignment, you will learn to generate data for [Catalan objects](https://en.wikipedia.org/wiki/Catalan_number). The Catalan numbers are a famous sequence of numbers in combinatorics and have lots of different but equivalent interpretations. In fact, there is a whole book on them titled "[Catalan Numbers](https://www.amazon.com/Catalan-Numbers-Richard-P-Stanley/dp/1107427746)" by a well known combinatorist - Richard P. Stanley.
+
+The 4 types of Catalan objects you will generate data for are:
+* Matching parenthesizations
+* Associative orders on products
+* 231-avoiding permutations
+* Triangulations of a polygon
+
+Furthermore, for each object you generate, you will also record the time it takes to generate the objects and plot a graph to see how the time required varies with $n$. In theory, all Catalan objects can be generated equally quickly, but you may find that the simplest approach for some objects is less efficient than others!
+
+If you are interested in learning more, there are beautiful connections between the Catalan numbers and a polytope called the [associahedron](https://en.wikipedia.org/wiki/Associahedron). Furthermore, you can impose additional structure on certain Catalan objects to obtain something called the [Tamari lattice](https://en.wikipedia.org/wiki/Tamari_lattice).
---
+++ 6_18_2024_catalan/catalan.py
@@ -0,0 +1,88 @@
+import itertools
+
+def parenthesizations(n):
+  """
+  Returns a list of all possible parenthesizations of length n.
+
+  Parameters:
+    n (int): The length of the parenthesizations.
+
+  Returns:
+    A list of strings, where each inner string represents a valid parenthesization of length n.
+  
+  Example:
+  >>> parenthesizations(3)
+  {'((()))', '(()())', '(())()', '()(())', '()()()'}
+  """
+  if n == 0:
+    return {""}
+  else:
+    # TODO
+    pass
+
+def product_orders(n):
+  """
+  Returns a list of all possible ways to multiply of n elements.
+
+  Parameters:
+    n (int): The number of elements multiplied.
+
+  Returns:
+    A set of strings where each string represents a way to multiply n elements.
+  
+  Example:
+  >>> product_orders(4)
+  {'((?*?)*?)*?', '(?*(?*?))*?', '(?*?)*(?*?)', '?*((?*?)*?)', '?*(?*(?*?))'}
+  """
+  if n == 0:
+    return {""}
+  elif n == 1:
+    return {"?"}
+  elif n == 2:
+    return {"?*?"}
+  else:
+    # TODO
+    pass
+
+def permutations_avoiding_231(n):
+  """
+  Returns a list of permutations of length n avoiding the pattern 2-3-1.
+  
+  Parameters:
+    n (int): The length of the permutation.
+  
+  Returns:
+    A list of permutations of length n that do not contain the pattern 2-3-1.
+  
+  Example:
+  >>> permutations_avoiding_231(4)
+  {(1, 2, 3, 4), (1, 2, 4, 3), (1, 3, 2, 4), (1, 4, 2, 3), (1, 4, 3, 2), (2, 1, 3, 4), (2, 1, 4, 3), (3, 1, 2, 4), (3, 2, 1, 4), (4, 1, 2, 3), (4, 1, 3, 2), (4, 2, 1, 3), (4, 3, 1, 2), (4, 3, 2, 1)}
+  """
+  if n < 3:
+    return set(itertools.permutations(range(1, n+1)))
+  else:
+    # TODO
+    pass
+
+def triangulations(n):
+  """
+  Returns a list of all possible triangulations of an n-sided polygon. A triangulation
+  is represented as a list of internal edges. Vertices are labeled 0 through n-1 clockwise.
+
+  Parameters:
+    n (int): The number of sides of the polygon.
+
+  Returns:
+    A set of tuple of pairs, where each pair represents an internal edge in the triangulation.
+  
+  Example:
+  >>> triangulations(3)
+  {((0, 3), (1, 3)), ((1, 4), (2, 4)), ((1, 3), (1, 4)), ((0, 2), (2, 4)), ((0, 2), (0, 3))}
+  """
+  if n < 3:
+    return set()
+  elif n == 3:
+    return {tuple()}
+  else:
+    pass
+    # TODO
