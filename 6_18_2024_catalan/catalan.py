c1c9fdabae0ae352fe4ecb6eea8fc46448a545ab
Said A. Sheck <said1210@uw.edu>
2024-06-19 22:44:04 -0700

    final submisson

--- 6_18_2024_catalan/catalan.py
+++ 6_18_2024_catalan/catalan.py
@@ -17,8 +17,22 @@ def parenthesizations(n):
   if n == 0:
     return {""}
   else:
-    # TODO
-    pass
+    results = set()
+    def createParenth(curr_string, open_count, close_count):
+      # Base Case where if we have equal brackets then we can say its closed
+      if (open_count == n and close_count == n):
+        results.add(curr_string)
+        return
+      
+      if open_count < n :
+        createParenth(curr_string + "(", open_count + 1, close_count)
+      
+      if close_count < open_count:
+        createParenth(curr_string + ")", open_count, close_count + 1)
+    createParenth("", 0, 0)
+    return results
+
+  
 
 def product_orders(n):
   """
@@ -34,6 +48,7 @@ def product_orders(n):
   >>> product_orders(4)
   {'((?*?)*?)*?', '(?*(?*?))*?', '(?*?)*(?*?)', '?*((?*?)*?)', '?*(?*(?*?))'}
   """
+
   if n == 0:
     return {""}
   elif n == 1:
@@ -41,8 +56,28 @@ def product_orders(n):
   elif n == 2:
     return {"?*?"}
   else:
-    # TODO
-    pass
+    results = set()
+    for k in range(1, n):
+      leftOrders = product_orders(k)
+      rightOrders = product_orders(n - k)
+      for left in leftOrders:
+        for right in rightOrders:
+          results.add(f'({left}*{right})')
+    return results
+  
+
+
+# Helper Function to check if a subsequence follows a 2 - 3 - 1 ordering
+def permCondition(subsequence):
+  # Check if the first digit is the second largest
+  if subsequence[0] == sorted(subsequence)[-2]:
+    #Check if the second digit is the largest
+    if subsequence[1] == max(subsequence):
+      # Check if the last digit is the smallest
+      if subsequence[2] == min(subsequence):
+        return False
+  return True
+
 
 def permutations_avoiding_231(n):
   """
@@ -61,8 +96,17 @@ def permutations_avoiding_231(n):
   if n < 3:
     return set(itertools.permutations(range(1, n+1)))
   else:
-    # TODO
-    pass
+    # Collect Valid Permutations
+    validPerms = set()
+    
+    # Go through every permutation
+    for perm in itertools.permutations(range(1, n+1)):
+      # Check if they have 231 pattern within any subsequence
+      if permCondition(perm):
+        # If a permuation comes up clean add it to our list
+        validPerms.add(perm)
+    return validPerms
+    
 
 def triangulations(n):
   """
@@ -76,7 +120,7 @@ def triangulations(n):
     A set of tuple of pairs, where each pair represents an internal edge in the triangulation.
   
   Example:
-  >>> triangulations(3)
+  >>> triangulations(3) 
   {((0, 3), (1, 3)), ((1, 4), (2, 4)), ((1, 3), (1, 4)), ((0, 2), (2, 4)), ((0, 2), (0, 3))}
   """
   if n < 3:
@@ -84,5 +128,13 @@ def triangulations(n):
   elif n == 3:
     return {tuple()}
   else:
-    pass
-    # TODO
+    result = set()
+    for k in range(1, n-1):
+      left_triangulations = triangulations(k)
+      right_triangulations = triangulations(n - k)
+      for left in left_triangulations:
+        for right in right_triangulations:
+          result.add((left[0], right[0]))
+          result.add((left[1], right[0]))
+    return result
+   
